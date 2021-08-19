9b881b80df0589373a5fb78a8a2664821fcb6059
Foivos Zakkak <fzakkak@redhat.com>
2021-08-20 01:16:16 +0300

    Replicate Quarkus test for 21.3

--- Readme.md
+++ Readme.md
@@ -2,11 +2,11 @@
 
 ```
 cd /tmp
-git clone --branch serialization-registration https://github.com/zakkak/issue-reproducers reproducers
+git clone --branch serialization-registration-21.3 https://github.com/zakkak/issue-reproducers reproducers
 cd reproducers
 mvn package
-export JAVA_HOME=/opt/jvms/graalvm-ce-java11-21.2.0
-$JAVA_HOME/bin/native-image --initialize-at-build-time \
+export JAVA_HOME=/opt/jvms/graalvm-ce-java11-21.3.0-dev
+$JAVA_HOME/bin/native-image --initialize-at-build-time --diagnostics-mode \
   --no-fallback -H:+ReportExceptionStackTraces \
   -jar target/reproducer-1.0-SNAPSHOT.jar
 ./reproducer-1.0-SNAPSHOT
--- src/main/java/SerializationFeature.java
+++ src/main/java/SerializationFeature.java
@@ -1,7 +1,7 @@
-import com.oracle.svm.core.annotate.*;
-import com.oracle.svm.core.jdk.serialize.SerializationRegistry;
+import com.oracle.svm.core.annotate.AutomaticFeature;
 import com.oracle.svm.reflect.serialize.SerializationSupport;
 import com.oracle.svm.util.ReflectionUtil;
+import org.graalvm.home.Version;
 import org.graalvm.nativeimage.ImageSingletons;
 import org.graalvm.nativeimage.hosted.Feature;
 import sun.reflect.ReflectionFactory;
@@ -28,34 +28,56 @@ public class SerializationFeature implements Feature {
         }
     }
 
-    private static void registerSerializationForClass(Class<?> theClass) throws ClassNotFoundException, InvocationTargetException, IllegalAccessException {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        Class<?> objectClass = Class.forName("java.lang.Object", false, classLoader);
+    private static void registerSerializationForClass(Class<?> var0) throws ClassNotFoundException, InvocationTargetException, IllegalAccessException {
+        Version var1 = Version.getCurrent();
+        int[] var2 = new int[]{21};
+        if (var1.compareTo(var2) >= 0) {
+            ClassLoader var3 = Thread.currentThread().getContextClassLoader();
+            Class var25 = Class.forName("java.lang.Object", false, var3);
+            Version var5 = Version.getCurrent();
+            int[] var4 = new int[]{21, 3};
+            Class var6;
+            Class var8;
+            if (var5.compareTo(var4) < 0) {
+                var6 = Class.forName("com.oracle.svm.core.jdk.serialize.SerializationRegistry", false, var3);
+                var8 = Class.forName("com.oracle.svm.reflect.serialize.hosted.SerializationFeature", false, var3);
+            } else {
+                var6 = Class.forName("com.oracle.svm.reflect.serialize.SerializationRegistry", false, var3);
+                var8 = Class.forName("com.oracle.svm.reflect.serialize.hosted.SerializationBuilder", false, var3);
+            }
 
-        if (Class.forName("java.io.Externalizable", false, classLoader).isAssignableFrom(theClass)) {
-            Class<?> classClass = Class.forName("java.lang.Class", false, classLoader);
-            Method getExternalizableConstructor = ReflectionUtil.lookupMethod(ObjectStreamClass.class, "getExternalizableConstructor", classClass);
-            Class<?> constructorClass = ((Constructor<?>) getExternalizableConstructor.invoke(null, theClass)).getDeclaringClass();
-            com.oracle.svm.reflect.serialize.hosted.SerializationFeature.addReflections(theClass, constructorClass);
-            return;
+            Object var21 = ImageSingletons.lookup(var6);
+            Class[] var7 = new Class[]{Class.class, Class.class};
+            Method var15 = ReflectionUtil.lookupMethod(var8, "addReflections", var7);
+            ReflectionFactory var16 = ReflectionFactory.getReflectionFactory();
+            if (Class.forName("java.io.Externalizable", false, var3).isAssignableFrom(var0)) {
+                Class[] var9 = new Class[1];
+                Class var10 = Class.forName("java.lang.Class", false, var3);
+                var9[0] = var10;
+                Method var11 = ReflectionUtil.lookupMethod(ObjectStreamClass.class, "getExternalizableConstructor", var9);
+                Object[] var12 = new Object[]{var0};
+                Class var14 = ((Constructor)var11.invoke((Object)null, var12)).getDeclaringClass();
+                Class[] var13 = new Class[]{var0, var14};
+                var15.invoke((Object)null, (Object[])var13);
+                return;
+            }
+
+            Constructor var17;
+            if (!Modifier.isAbstract(var0.getModifiers())) {
+                var17 = var16.newConstructorForSerialization(var0);
+            } else {
+                var17 = var16.newConstructorForSerialization(SerializationSupport.StubForAbstractClass.class);
+            }
+
+            Class var22 = var17.getDeclaringClass();
+            Class[] var18 = new Class[0];
+            Method var19 = ReflectionUtil.lookupMethod(Constructor.class, "getConstructorAccessor", var18);
+            Object[] var20 = new Object[0];
+            Object var23 = var19.invoke(var17, var20);
+            ((SerializationSupport)var21).addConstructorAccessor(var0, var22, var23);
+            Class[] var24 = new Class[]{var0, var25};
+            var15.invoke((Object)null, (Object[])var24);
         }
-
-        ReflectionFactory reflectionFactory = ReflectionFactory.getReflectionFactory();
-        Constructor<?> constructor;
-        if (!Modifier.isAbstract(theClass.getModifiers())) {
-            constructor = reflectionFactory.newConstructorForSerialization(theClass);
-        } else {
-            constructor = reflectionFactory.newConstructorForSerialization(SerializationSupport.StubForAbstractClass.class);
-        }
-
-        Class<?> var18 = constructor.getDeclaringClass();
-        Class<?>[] var14 = new Class[0];
-        Method var15 = ReflectionUtil.lookupMethod(Constructor.class, "getConstructorAccessor", var14);
-        Object[] var16 = new Object[0];
-        Object var19 = var15.invoke(constructor, var16);
-        Object var17 = ImageSingletons.lookup(SerializationRegistry.class);
-        ((SerializationSupport)var17).addConstructorAccessor(theClass, var18, var19);
-        com.oracle.svm.reflect.serialize.hosted.SerializationFeature.addReflections(theClass, objectClass);
     }
 
 
