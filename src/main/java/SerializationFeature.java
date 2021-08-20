f224456903691f4134793a2600a1cad864ec8ea9
Foivos Zakkak <fzakkak@redhat.com>
2021-08-20 14:44:36 +0300

    Use RuntimeSerialization.register with 21.3

--- src/main/java/SerializationFeature.java
+++ src/main/java/SerializationFeature.java
@@ -32,51 +32,54 @@ public class SerializationFeature implements Feature {
         Version var1 = Version.getCurrent();
         int[] var2 = new int[]{21};
         if (var1.compareTo(var2) >= 0) {
-            ClassLoader var3 = Thread.currentThread().getContextClassLoader();
-            Class var25 = Class.forName("java.lang.Object", false, var3);
-            Version var5 = Version.getCurrent();
-            int[] var4 = new int[]{21, 3};
-            Class var6;
-            Class var8;
-            if (var5.compareTo(var4) < 0) {
-                var6 = Class.forName("com.oracle.svm.core.jdk.serialize.SerializationRegistry", false, var3);
-                var8 = Class.forName("com.oracle.svm.reflect.serialize.hosted.SerializationFeature", false, var3);
-            } else {
-                var6 = Class.forName("com.oracle.svm.reflect.serialize.SerializationRegistry", false, var3);
-                var8 = Class.forName("com.oracle.svm.reflect.serialize.hosted.SerializationBuilder", false, var3);
-            }
-
-            Object var21 = ImageSingletons.lookup(var6);
-            Class[] var7 = new Class[]{Class.class, Class.class};
-            Method var15 = ReflectionUtil.lookupMethod(var8, "addReflections", var7);
-            ReflectionFactory var16 = ReflectionFactory.getReflectionFactory();
-            if (Class.forName("java.io.Externalizable", false, var3).isAssignableFrom(var0)) {
-                Class[] var9 = new Class[1];
-                Class var10 = Class.forName("java.lang.Class", false, var3);
-                var9[0] = var10;
-                Method var11 = ReflectionUtil.lookupMethod(ObjectStreamClass.class, "getExternalizableConstructor", var9);
-                Object[] var12 = new Object[]{var0};
-                Class var14 = ((Constructor)var11.invoke((Object)null, var12)).getDeclaringClass();
-                Class[] var13 = new Class[]{var0, var14};
-                var15.invoke((Object)null, (Object[])var13);
+            ClassLoader var5 = Thread.currentThread().getContextClassLoader();
+            Version var4 = Version.getCurrent();
+            int[] var3 = new int[]{21, 3};
+            if (var4.compareTo(var3) >= 0) {
+                Class var6 = Class.forName("org.graalvm.nativeimage.hosted.RuntimeSerialization", false, var5);
+                Class[] var7 = new Class[]{Class[].class};
+                Method var10 = ReflectionUtil.lookupMethod(var6, "register", var7);
+                Object[] var8 = new Object[1];
+                Class[] var9 = new Class[]{var0};
+                var8[0] = var9;
+                var10.invoke((Object)null, var8);
                 return;
             }
 
-            Constructor var17;
-            if (!Modifier.isAbstract(var0.getModifiers())) {
-                var17 = var16.newConstructorForSerialization(var0);
-            } else {
-                var17 = var16.newConstructorForSerialization(SerializationSupport.StubForAbstractClass.class);
+            Class var30 = Class.forName("java.lang.Object", false, var5);
+            Class var11 = Class.forName("com.oracle.svm.core.jdk.serialize.SerializationRegistry", false, var5);
+            Class var13 = Class.forName("com.oracle.svm.reflect.serialize.hosted.SerializationFeature", false, var5);
+            Object var26 = ImageSingletons.lookup(var11);
+            Class[] var12 = new Class[]{Class.class, Class.class};
+            Method var20 = ReflectionUtil.lookupMethod(var13, "addReflections", var12);
+            ReflectionFactory var21 = ReflectionFactory.getReflectionFactory();
+            if (Class.forName("java.io.Externalizable", false, var5).isAssignableFrom(var0)) {
+                Class[] var14 = new Class[1];
+                Class var15 = Class.forName("java.lang.Class", false, var5);
+                var14[0] = var15;
+                Method var16 = ReflectionUtil.lookupMethod(ObjectStreamClass.class, "getExternalizableConstructor", var14);
+                Object[] var17 = new Object[]{var0};
+                Class var19 = ((Constructor)var16.invoke((Object)null, var17)).getDeclaringClass();
+                Class[] var18 = new Class[]{var0, var19};
+                var20.invoke((Object)null, (Object[])var18);
+                return;
             }
 
-            Class var22 = var17.getDeclaringClass();
-            Class[] var18 = new Class[0];
-            Method var19 = ReflectionUtil.lookupMethod(Constructor.class, "getConstructorAccessor", var18);
-            Object[] var20 = new Object[0];
-            Object var23 = var19.invoke(var17, var20);
-            ((SerializationSupport)var21).addConstructorAccessor(var0, var22, var23);
-            Class[] var24 = new Class[]{var0, var25};
-            var15.invoke((Object)null, (Object[])var24);
+            Constructor var22;
+            if (!Modifier.isAbstract(var0.getModifiers())) {
+                var22 = var21.newConstructorForSerialization(var0);
+            } else {
+                var22 = var21.newConstructorForSerialization(SerializationSupport.StubForAbstractClass.class);
+            }
+
+            Class var27 = var22.getDeclaringClass();
+            Class[] var23 = new Class[0];
+            Method var24 = ReflectionUtil.lookupMethod(Constructor.class, "getConstructorAccessor", var23);
+            Object[] var25 = new Object[0];
+            Object var28 = var24.invoke(var22, var25);
+            ((SerializationSupport)var26).addConstructorAccessor(var0, var27, var28);
+            Class[] var29 = new Class[]{var0, var30};
+            var20.invoke((Object)null, (Object[])var29);
         }
     }
 
