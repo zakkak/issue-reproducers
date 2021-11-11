eaa5443559f0ab2565067686d3001125d0388ac0
Foivos Zakkak <fzakkak@redhat.com>
2021-11-11 10:07:38 +0200

    Initial commit

--- Readme.md
+++ Readme.md
@@ -2,13 +2,12 @@
 
 ```
 cd /tmp
-git clone --branch record-annotations-21.3 https://github.com/zakkak/issue-reproducers reproducers
+git clone --branch krb5-credentials-failure-java17 https://github.com/zakkak/issue-reproducers reproducers
 cd reproducers
-mvn package
 export JAVA_HOME=/opt/jvms/graalvm-ce-java17-21.3.0
+mvn package
 $JAVA_HOME/bin/native-image \
   --no-fallback -H:+ReportExceptionStackTraces \
-  -H:ReflectionConfigurationFiles=./META-INF/native-image/reflect-config.json \
   -jar target/reproducer-1.0-SNAPSHOT.jar
 ./reproducer-1.0-SNAPSHOT
 ```
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,14 @@
-import java.lang.annotation.Annotation;
-import java.lang.reflect.RecordComponent;
-
 public class Main {
 
 	public static void main(String[] args) {
-		RecordComponent[] recordComponents = F.class.getRecordComponents();
-		for (RecordComponent component: recordComponents) {
-			System.out.println(component);
-			Annotation[] annotations = component.getAnnotations();
-			for (Annotation annotation : annotations) {
-				System.out.println(annotation);
-			}
-		}
-
-		F x = new F("x");
-		System.out.println(x);
-	}
-
-	static record F(String name) {
+		try {
+			@SuppressWarnings({"nullness"})
+			sun.security.krb5.Credentials credentials =
+				sun.security.krb5.Credentials.acquireTGTFromCache(null, null);
+			System.out.println(credentials != null);
+		  } catch (Exception ex) {
+			System.out.println("FAILED");
+		  }
+		;
 	}
 }
