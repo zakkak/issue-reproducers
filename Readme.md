f87ddd83ebb678fad4652f88dba6156bbb1b40ea
Foivos Zakkak <fzakkak@redhat.com>
2022-01-19 14:57:59 +0200

    Reproducer for quarkusio/quarkus/issues/14089

--- Readme.md
+++ Readme.md
@@ -2,13 +2,12 @@
 
 ```
 cd /tmp
-git clone --branch sqlite-compilation-npe https://github.com/zakkak/issue-reproducers reproducers
+git clone --branch isolates-hardcoded-properties https://github.com/zakkak/issue-reproducers reproducers
 cd reproducers
 export JAVA_HOME=/opt/jvms/graalvm-ce-java11-21.3.0
 mvn package
 $JAVA_HOME/bin/native-image \
   --no-fallback -H:+ReportExceptionStackTraces \
-  -H:-ParseOnce \
   -jar target/reproducer-1.0-SNAPSHOT.jar
-./reproducer-1.0-SNAPSHOT
+./reproducer-1.0-SNAPSHOT -Dproperty.to.override=42
 ```
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,29 @@
-import java.sql.DriverManager;
+import org.graalvm.nativeimage.IsolateThread;
+import org.graalvm.nativeimage.Isolates;
+import org.graalvm.nativeimage.c.function.CEntryPoint;
 
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Hello");
+		String property = System.getProperty("property.to.override");
+		System.out.println("property = " + property);
+
+		var params = Isolates.CreateIsolateParameters.getDefault();
+		var isolateThread = Isolates.createIsolate(params);
+
 		try {
-			Class.forName("org.sqlite.JDBC");
-			DriverManager.getConnection("jdbc:sqlite:./temp-db").createStatement();
-			System.out.println("SUCCESS");
-    	} catch (Throwable t) {
-			System.out.println("FAILED");
+			System.out.println("isolated-property " + processInIsolate(isolateThread));
 		}
-    }
+		finally {
+			Isolates.tearDownIsolate(isolateThread);
+		}
+	}
+
+	@CEntryPoint
+	private static int processInIsolate(@CEntryPoint.IsolateThreadContext IsolateThread isolateThread) {
+		String property = System.getProperty("property.to.override");
+		System.out.println("isolated-property = " + property);
+		return Integer.parseInt(property);
+	}
 }
