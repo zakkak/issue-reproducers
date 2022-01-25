1cb8550aa43af7a041433ee89f9689397de54dc4
Foivos Zakkak <fzakkak@redhat.com>
2022-01-25 12:46:10 +0200

    simplify

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,7 @@
-
-/*
- * Copyright (c) 2019 Matthias Sohn <matthias.sohn@sap.com>
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Distribution License v. 1.0 which is available at
- * https://www.eclipse.org/org/documents/edl-v10.php.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
 import java.lang.management.ManagementFactory;
 
 import javax.management.InstanceAlreadyExistsException;
-import javax.management.InstanceNotFoundException;
 import javax.management.MBeanRegistrationException;
-import javax.management.MBeanServer;
 import javax.management.MalformedObjectNameException;
 import javax.management.NotCompliantMBeanException;
 import javax.management.ObjectName;
@@ -21,18 +9,14 @@ import javax.management.ObjectName;
 public class Main {
 
 	public static void main(String[] args) {
-		Object mbean = new Object();
-		MBeanServer server = ManagementFactory.getPlatformMBeanServer();
 		try {
-			ObjectName mbeanName = new ObjectName("objectName");
-			if (server.isRegistered(mbeanName)) {
-				server.unregisterMBean(mbeanName);
-			}
-			server.registerMBean(mbean, mbeanName);
+			ManagementFactory.getPlatformMBeanServer()
+			                 .registerMBean(new Hello(), new ObjectName("test:type=Hello"));
+			System.out.println("SUCCESS");
 		} catch (MalformedObjectNameException | InstanceAlreadyExistsException
-				| MBeanRegistrationException | NotCompliantMBeanException
-				| InstanceNotFoundException e) {
+				| MBeanRegistrationException | NotCompliantMBeanException e) {
 			System.err.println(e.getMessage());
+			e.printStackTrace(System.err);
 		}
     }
 }
