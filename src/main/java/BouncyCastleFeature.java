3eceeb440ae1eae4737bb364c9447cf5361e921f
Foivos Zakkak <fzakkak@redhat.com>
2022-02-11 14:47:52 +0200

    Test provider addition ordering

--- src/main/java/BouncyCastleFeature.java
+++ src/main/java/BouncyCastleFeature.java
@@ -1,4 +1,5 @@
 import com.oracle.svm.core.annotate.*;
+
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeClassInitialization;
@@ -10,10 +11,34 @@ public class BouncyCastleFeature implements Feature {
 
     @Override
     public void afterRegistration(AfterRegistrationAccess access) {
-        RuntimeClassInitialization.initializeAtBuildTime("org.bouncycastle");
-        RuntimeClassInitialization.initializeAtRunTime("org.bouncycastle.jcajce.provider.drbg.DRBG$Default");
-        RuntimeClassInitialization.initializeAtRunTime("org.bouncycastle.jcajce.provider.drbg.DRBG$NonceAndIV");
-        Security.addProvider(new BouncyCastleProvider());
+        System.out.println("After Registration Hello!!!!");
+
+        // RuntimeClassInitialization.initializeAtBuildTime("org.bouncycastle");
+        // RuntimeClassInitialization.initializeAtRunTime("org.bouncycastle.crypto.CryptoServicesRegistrar");
+        // RuntimeClassInitialization.initializeAtRunTime("org.bouncycastle.jcajce.provider.drbg.DRBG$Default");
+        // RuntimeClassInitialization.initializeAtRunTime("org.bouncycastle.jcajce.provider.drbg.DRBG$NonceAndIV");
+
+        // Security.addProvider(new BouncyCastleProvider());
     }
 
+    @Override
+    public void beforeAnalysis(BeforeAnalysisAccess access) {
+        System.out.println("BeforeAnalysis Hello!!!!");
+
+        RuntimeClassInitialization.initializeAtBuildTime("org.bouncycastle");
+        RuntimeClassInitialization.initializeAtRunTime("org.bouncycastle.crypto.CryptoServicesRegistrar");
+        RuntimeClassInitialization.initializeAtRunTime("org.bouncycastle.jcajce.provider.drbg.DRBG$Default");
+        RuntimeClassInitialization.initializeAtRunTime("org.bouncycastle.jcajce.provider.drbg.DRBG$NonceAndIV");
+
+        // Security.addProvider(new BouncyCastleProvider());
+    }
+
+    @Override
+    public void afterAnalysis(AfterAnalysisAccess access) {
+        System.out.println("AfterAnalysis Hello!!!!");
+
+        // Too late... providers need to be registered before the analysis
+
+        // Security.addProvider(new BouncyCastleProvider());
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,30 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
+
+// import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.Security;
 
 public class Main {
 
-    public static void main(String[] args) {
+    static {
+        System.out.println("Hello!!!!");
+
+        // Too late... providers need to be registered before the analysis, but
+        // static initialization takes plave after it.
+
         Security.addProvider(new BouncyCastleProvider());
+    }
+
+    public static void main(String[] args) {
 
         try {
-            Cipher rsaInstance = Cipher.getInstance("RSA", "BC");
+            // KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA", "BC");
+            // keyPairGenerator.generateKeyPair();
+            // System.out.println("Success");
+            Cipher rsaInstance = Cipher.getInstance("AES/CBC/PKCS7Padding", "BC");
             System.out.println(rsaInstance.getAlgorithm());
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | NoSuchProviderException e) {
             e.printStackTrace();
