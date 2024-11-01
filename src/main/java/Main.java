96e655e9929126a0154b40fac32f3ad610162a96
Foivos Zakkak <fzakkak@redhat.com>
2024-11-01 12:59:06 +0200

    jep472 reproducer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
+import com.aayushatharva.brotli4j.Brotli4jLoader;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Brotli4j version: " + Brotli4jLoader.isAvailable());
     }
 }
