7fd8492bfa0a05dd33c43713e9a8e6c573dbd1bd
Foivos Zakkak <fzakkak@redhat.com>
2024-11-14 20:31:11 +0200

    reproducer for folder resources

--- Readme.md
+++ Readme.md
@@ -1,28 +1,13 @@
-# Reproducer for native-image issue
+# Reproducer for non native-image issue
 
-Reproducer of https://github.com/oracle/graal/issues/10008
+Reproducer of https://github.com/quarkusio/quarkus/issues/44478
 
 ```
 cd /tmp
-git clone --branch 2024-11-01-jep472-manifest-ignored https://github.com/zakkak/issue-reproducers reproducers
+git clone --branch2024-11-14-directory-resource https://github.com/zakkak/issue-reproducers reproducers
 cd reproducers
 mvn package
 # run in JVM
 $JAVA_HOME/java \
   -jar target/reproducer-1.0-SNAPSHOT.jar
-# generate native-image
-$JAVA_HOME/native-image \
-  --no-fallback \
-  --initialize-at-build-time=.  \
-  -jar target/reproducer-1.0-SNAPSHOT.jar
-# Build prints warning about native access
-```
-
-The warning is:
-  
-```
-WARNING: A restricted method in java.lang.System has been called
-WARNING: java.lang.System::loadLibrary has been called by com.aayushatharva.brotli4j.Brotli4jLoader in an unnamed module (file:/home/zakkak/code/issue-reproducers/target/reproducer-1.0-SNAPSHOT.jar)
-WARNING: Use --enable-native-access=ALL-UNNAMED to avoid a warning for callers in this module
-WARNING: Restricted methods will be blocked in a future release unless native access is enabled
 ```
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,37 @@
-import com.aayushatharva.brotli4j.Brotli4jLoader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Brotli4j version: " + Brotli4jLoader.isAvailable());
+        List<String> greetings = new ArrayList<>();
+        try (InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("configs");
+            BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+            System.err.println("Scanning configs classpath folder");
+
+            String filename;
+            while ((filename = br.readLine()) != null) {
+                System.err.println("Reading config file" + filename);
+                try (
+                    InputStream in2 = Thread.currentThread().getContextClassLoader().getResourceAsStream("configs/" + filename);
+                    BufferedReader br2 = new BufferedReader(new InputStreamReader(in2))) {
+                    String greeting;
+                    while ((greeting = br2.readLine()) != null) {
+                        greetings.add(greeting);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+        
+        if (greetings.isEmpty()) {
+            throw new IllegalStateException("Greeting service has no greetings");
+        }
     }
 }
